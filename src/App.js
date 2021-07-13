cff63f148a9ee5e30f65df372b74c4311f91eec4
Anonymous-Chris <sk.niyara@gmail.com>
2021-07-12 21:09:24 -0500

    updating portfolio

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
-
+import Tejan from "./imgs/tejan.jpg";
 function App() {
   return (
     <div className="App">
       <p>Tejan dai portfolio</p>
+      <p>Tejan is an intelligent guy</p>
+      <img src={Tejan} alt="image_brother" />
     </div>
   );
 }
