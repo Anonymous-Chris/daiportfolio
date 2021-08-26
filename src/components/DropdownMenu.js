6704b037b818c26a4eb7f34f58a1c4d3b006d902
Anonymous-Chris <sk.niyara@gmail.com>
2021-08-26 16:21:55 -0500

    add navbar

---
+++ src/components/DropdownMenu.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function DropdownMenu() {
+  function DropdownItem(props) {
+    return (
+      <a href="#" className="menu-item">
+        <span className="icon-button">{props.leftIcon}</span>
+        {props.children}
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
+    );
+  }
+
+  return (
+    <div className="dropdown">
+      <DropdownItem>Foo</DropdownItem>
+      <DropdownItem leftIcon={<BoltIcon />}>Bar</DropdownItem>
+    </div>
+  );
+}
+
+export default DropdownMenu;
